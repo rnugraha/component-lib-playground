d30936d4bb7fb111c83b9395bceccf7a0dffd51f
Riza Nugraha <rnugraha@gmail.com>
2017-11-01 19:07:53 +0100

    Include react-select-checked in package.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { CheckedSelect } from 'react-select-checked'
+import { CheckedSelect } from 'react-select-checked';
 
 import '../assets/stylesheets/base.scss';
 
 
 const FLAVOURS = [
-    { label: 'Chocolate', value: 'chocolate', disabled: true},
-    { label: 'Vanilla', value: 'vanilla' },
-    { label: 'Strawberry', value: 'strawberry', isSelected: true},
-    { label: 'Caramel', value: 'caramel', isSelected: true},
-    { label: 'Cookies and Cream', value: 'cookiescream' },
-    { label: 'Peppermint', value: 'peppermint' },
-    { label: 'Coconut', value: 'coconut', },
+    {label: 'Chocolate', value: 'chocolate', disabled: true},
+    {label: 'Vanilla', value: 'vanilla'},
+    {label: 'Strawberry', value: 'strawberry', isSelected: true},
+    {label: 'Caramel', value: 'caramel', isSelected: true},
+    {label: 'Cookies and Cream', value: 'cookiescream'},
+    {label: 'Peppermint', value: 'peppermint'},
+    {label: 'Coconut', value: 'coconut',},
 ];
 
 function logChange(val) {
     console.log('Selected value: ', val);
 }
 
-const App = ({ name }) => {
-
-  return (
-    <div>
-      <h1>Hello, {name}!</h1>
-        <CheckedSelect
-            onChange={logChange}
-            options={FLAVOURS}
-            placeholder="Select your favourite(s)"
-        />
-    </div>
-  );
+const App = ({name}) => {
+    const containerStyle = {
+        width: '400px',
+    };
+    return (
+        <div>
+            <h1>Hello, {name}!</h1>
+            <div style={containerStyle}>
+                <CheckedSelect
+                    onChange={logChange}
+                    options={FLAVOURS}
+                    placeholder="Select your favourite(s)"
+                />
+            </div>
+        </div>
+    );
 };
 
 App.propTypes = {
-  name: PropTypes.string,
+    name: PropTypes.string,
 };
 
 export default App;
